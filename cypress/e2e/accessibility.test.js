6d14ef90d31171a9128c024d86c5382bba41022d
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2020-11-23 19:25:20 +0700

    chore: add e2e test with cypress

---
+++ cypress/e2e/accessibility.test.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+    cy.visit("/").get("#___gatsby").injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+})
