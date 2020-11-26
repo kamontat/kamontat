411555e2662cacd9c0f4f362943b8e4db69ad472
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-11-26 17:13:04 +0700

    refac: formatted code via eslint --fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,40 @@
 /* eslint-disable */
-const ts = require("gatsby-plugin-ts-config");
+
+const ts = require("gatsby-plugin-ts-config")
+const isDev = process.env.NODE_ENV === "development"
 
 module.exports = ts.generateConfig({
   projectRoot: process.cwd(),
   configDir: "gatsby",
   babel: {
-    "plugins": [
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      [
+        "@emotion",
+        {
+          cssPropOptimization: true,
+          sourceMap: isDev,
+          autoLabel: "dev-only",
+          labelFormat: "[filename]-[local]",
+        },
+      ],
       [
         "@emotion/babel-plugin-jsx-pragmatic",
         {
-          "export": "jsx",
-          "import": "__cssprop",
-          "module": "@emotion/react"
-        }
+          export: "jsx",
+          import: "__cssprop",
+          module: "@emotion/react",
+        },
       ],
       [
         "@babel/plugin-transform-react-jsx",
         {
-          "pragma": "__cssprop",
-          "pragmaFrag": "React.Fragment"
-        }
-      ]
-    ]
-  }
-});
+          pragma: "__cssprop",
+          pragmaFrag: "React.Fragment",
+        },
+      ],
+    ],
+  },
+})
 
 /* eslint-enable */
