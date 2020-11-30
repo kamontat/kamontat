50949a91c459f341d6581a5bba80921f55f98e58
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-11-30 18:39:54 +0700

    perf(ui): add avatar, title and definition in index page

---
+++ src/components/index/Definition.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import tw, { styled } from "twin.macro"
+
+import { BaseOptions } from "../../layout/Base"
+
+interface DefinitionOptions extends BaseOptions {
+  message?: string
+}
+
+const Message = styled.h3(() => [tw`pb-3 font-sans text-lg`])
+
+export const Definition = ({ message }: DefinitionOptions): JSX.Element => {
+  if (message) return <Message>{message}</Message>
+  else return <span></span>
+}
---
+++ src/components/index/Title.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import tw, { styled } from "twin.macro"
+
+import { BaseOptions } from "../../layout/Base"
+
+interface TitleOptions extends BaseOptions {
+  name?: string
+  shortname?: string
+}
+
+// const Name = styled.h1(() => [tw`font-sans text-xl lg:text-2xl `])
+const Name = styled.h1(() => [tw`pt-4 m-1`, tw`font-sans text-xl lg:text-2xl`])
+const Highlight = styled.span(() => [tw`font-semibold hover:font-bold`])
+
+export const Title = ({ name, shortname }: TitleOptions): JSX.Element => {
+  if (name && shortname)
+    return (
+      <Name>
+        <Highlight>{name}</Highlight> ({shortname})
+      </Name>
+    )
+  else if (name) {
+    return (
+      <Name>
+        <Highlight>{name}</Highlight>
+      </Name>
+    )
+  } else if (shortname) {
+    return <Name>{shortname}</Name>
+  } else {
+    return <span></span>
+  }
+}
