8b1f71dc50ae1b08dfedb1d589faadd4d945cf07
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-11-30 18:39:18 +0700

    fix(ui): add new extra tailwind config

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -13,19 +13,24 @@ module.exports = {
       serif: ["Merriweather", "serif"],
     },
     extend: {
-      spacing: {
-        128: "32rem",
-        144: "36rem",
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
+        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
+        popup: "cubic-bezier(0.68, -0.55, 0.265, 1.55)",
       },
-      borderRadius: {
-        "4xl": "2rem",
+      zIndex: {
+        1: 1,
+        2: 2,
+        5: 5,
+      },
+      rotate: {
+        135: "135deg",
       },
     },
   },
   variants: {
     extend: {
       borderColor: ["focus-visible"],
-      opacity: ["disabled"],
     },
   },
 }
