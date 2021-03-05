03d82c3d7102a962f422626a046920d812dd3a7f
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-03-06 01:01:54 +0700

    chore(config): improve graphql codegen plugin option

--- gatsby/plugins/graphqlgen.ts
+++ gatsby/plugins/graphqlgen.ts
@@ -1,11 +1,43 @@
 import { DefaultPluginOption } from "./base"
 
+export type GraphqlCodegenPluginOption = Record<string, string>
+
+export interface GraphqlCodegenPlugin {
+  resolve: string
+  options: GraphqlCodegenPluginOption
+}
+
+export interface GraphqlCodegenConfig {
+  typesPrefix: string
+}
+
+export interface GraphqlPluckModule {
+  name: string
+  identifier: string
+}
+
+export interface GraphqlPluckConfig {
+  globalGqlIdentifierName?: string
+  modules?: GraphqlPluckModule[]
+}
+
+export interface GraphqlAdditionalSchemas {
+  key: string
+  fileName?: string
+  documentPaths?: string[]
+  pluckConfig?: GraphqlPluckConfig
+  schema?: string[]
+  codegenConfig?: GraphqlCodegenConfig
+  codegenPlugins?: GraphqlCodegenPlugin[]
+}
+
 export interface GraphqlCodegenOptions extends DefaultPluginOption {
   codegen?: boolean
   documentPaths?: string[]
   fileName?: string
   codegenDelay?: number
   failOnError?: boolean
-  codegenPlugins?: string[]
-  additionalSchemas?: string[]
+  codegenConfig?: GraphqlCodegenConfig
+  codegenPlugins?: GraphqlCodegenPlugin[]
+  additionalSchemas?: GraphqlAdditionalSchemas[]
 }
