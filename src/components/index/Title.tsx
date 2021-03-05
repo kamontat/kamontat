ee86225f588e72c5fea7c135f693b45ca1028a73
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-03-05 17:40:57 +0700

    perf(ui): change title font to serif

--- src/components/index/Title.tsx
+++ src/components/index/Title.tsx
@@ -9,7 +9,7 @@ interface TitleOptions extends BaseOptions {
 }
 
 // const Name = styled.h1(() => [tw`font-sans text-xl lg:text-2xl `])
-const Name = styled.h1(() => [tw`pt-4 m-1`, tw`font-sans text-xl lg:text-2xl`])
+const Name = styled.h1(() => [tw`pt-4 m-1`, tw`font-serif text-xl lg:text-2xl`])
 const Highlight = styled.span(() => [tw`font-semibold hover:font-bold`])
 
 export const Title = ({ name, shortname }: TitleOptions): JSX.Element => {
