a3cde060406662b42b5ca71051e9ad54b3979a97
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-03-09 13:16:31 +0700

    refac: clean some code

--- src/features/intl/index.ts
+++ src/features/intl/index.ts
@@ -1,5 +1,10 @@
+export const THAI = "Thai"
+export const ENG = "English"
+
+export const UNKNOWN = "Unknown"
+
 export const getLanguageName = (code: string): string => {
-  if (code.toLowerCase().includes("th")) return "Thai"
-  else if (code.toLowerCase().includes("en")) return "English"
-  else return "Unknown"
+  if (code.toLowerCase().includes("th")) return THAI
+  else if (code.toLowerCase().includes("en")) return ENG
+  else return UNKNOWN
 }
