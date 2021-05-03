647f10266ad1ad59c1066acd832a273623ba87cf
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-05-04 00:46:01 +0700

    chore: change coming soon component to to be continue

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
 // TODO: Implement coming soon component
-const ComingSoon = (): JSX.Element => <div>Hello world</div>
+const ComingSoon = (): JSX.Element => <div>To be continue</div>
 
 export default ComingSoon
