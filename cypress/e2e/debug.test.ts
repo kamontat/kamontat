467441584937deffbe82d82c487ceaf4a44b29e3
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-05-04 10:24:40 +0700

    chore: increase timeout in e2e test

--- cypress/e2e/debug.test.ts
+++ cypress/e2e/debug.test.ts
@@ -1,6 +1,6 @@
 describe("Debug page tests", () => {
   beforeEach(() => {
-    cy.visit("/debug", { timeout: 3000 }).get("main").injectAxe()
+    cy.visit("/debug", { timeout: 5000 }).get("main").injectAxe()
   })
 
   it("Has no detectable accessibility violations on load", () => {
--- cypress/e2e/index.test.ts
+++ cypress/e2e/index.test.ts
@@ -1,6 +1,6 @@
 describe("Index page tests", () => {
   beforeEach(() => {
-    cy.visit("/", { timeout: 3000 }).get("main").injectAxe()
+    cy.visit("/", { timeout: 5000 }).get("main").injectAxe()
   })
 
   it("Has no detectable accessibility violations on load", () => {
